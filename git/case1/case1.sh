ab750d364eeeb987b24e1ea0520594148dc686d9
FREMAUX <pierre.fremaux@cbp-group.com>
2016-07-13 13:37:00 +0200

    cleaning the directory

---
+++ git/case1/case1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git checkout -b branch-alice
+echo alice message 1 > file-alice.txt
+git add file*
+git commit -m "alice : init my branch"
+git checkout -b branch-bob
+echo bob message 1 > file-bob.txt
+git add file*
+git commit -m "bob : init my branch"
+git checkout -b branch-carol
+echo carol message 1 > file-alice.txt
+git add file*
+git commit -m "carol : init my branch"
