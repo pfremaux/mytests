dee9fe6f7aa3e56a7197856191c0578a12ced839
FREMAUX <pierre.fremaux@cbp-group.com>
2016-07-13 17:13:58 +0200

    adding cleaning script

--- git/case1/clean-case1.sh
+++ git/case1/clean-case1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-git checkout branch1-alice
+git checkout branch-alice
 git rm file*
-git checkout branch1-bob
+git checkout branch-bob
 git rm file*
-git checkout branch1-carol
+git checkout branch-carol
 git rm file*
 git checkout master
-git branch -d branch1-alice
-git branch -d branch1-bob
-git branch -d branch1-carol
+git branch -d branch-alice
+git branch -d branch-bob
+git branch -d branch-carol
 
