390df0e81d1fee170091ec28e5f46dca387e34ba
Pierre <pfremaux@gmail.com>
2016-09-05 00:02:30 +0200

    adjusting test files

--- bash/crypt/clean.sh
+++ bash/crypt/clean.sh
@@ -3,5 +3,3 @@ rm -rf a
 rm -rf b
 rm -rf c
 rm -rf U2*
-./test.sh
-./enc.sh a
--- bash/crypt/test.sh
+++ bash/crypt/test.sh
@@ -5,4 +5,9 @@ mkdir -p b
 mkdir -p c/3/cc
 mkdir c/3/dd
 mkdir c/4
+echo fichier n > c/4/aaa.txt
+echo fichier m > c/bbbb.txt
+echo fichier o > c/3/ccc.txt
+echo fichjier > c/3/dd/fdso.txt
+echo logggg > c/3/zzz.log
 
